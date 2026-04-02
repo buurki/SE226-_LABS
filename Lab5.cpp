08386eec39b6ae52f4f949fa2dcf9fab040314ba
buurki <161019315+buurki@users.noreply.github.com>
2026-04-02 13:01:39 +0300

    Add recursive function Quest3 to calculate result

--- Lab5.cpp
+++ Lab5.cpp
@@ -1,1 +1,24 @@
+QUESTION 4:
 
+#include <iostream>
+using namespace std;
+
+double Quest3(int n) {
+    if (n == 1)
+        return 1;
+
+    if (n % 2 == 0)
+        return Quest3(n - 1) - 1.0 / n;
+    else
+        return Quest3(n - 1) + 1.0 / n;
+}
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << "Result: " << Quest3(n) << endl;
+
+    return 0;
+}
