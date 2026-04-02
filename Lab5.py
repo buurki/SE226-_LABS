bc12065c0704a1e872e5a244d615032bc9292eb3
buurki <161019315+buurki@users.noreply.github.com>
2026-04-02 12:50:03 +0300

    Implement exponential function calculation
    Added functions to calculate the exponential of x using a Taylor series expansion.

--- Lab5.py
+++ Lab5.py
@@ -13,4 +13,25 @@ if __name__ == "__main__":
 
 QUESTION 2:
 
+def factorial(x):
+    if x == 0 or x == 1:
+        return 1
+    return x * factorial(x - 1)
+
+term = lambda x, n: (x ** n) / factorial(n)
+result = 0
+def exp_x(x, y):
+    global result
+    result = 0
+    for i in range(y):
+        result += term(x, i)
+    return result
+def main():
+    x = float(input("Enter x: "))
+    y = int(input("Enter number of terms: "))
+    print("Solution :", exp_x(x, y))
+if __name__ == "__main__":
+    main()
+
+
 
