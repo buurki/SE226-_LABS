2f90e4708272e61c931f32236a5ac77f77e93527
buurki <161019315+buurki@users.noreply.github.com>
2026-04-30 12:35:48 +0300

    Create lab7.py

---
+++ lab7.py
@@ -0,0 +1,14 @@
+lass Vehicle:
+    def __init__(self, vid, model, year):
+        self.vid = vid
+        self.model = model
+        self.year = year
+
+    def __str__(self):
+        return f"VID: {self.vid} | {self.model} ({self.year})"
+
+    def is_new(self, n):
+        return self.year >= 2026 - n
+
+    def __eq__(self, other):
+        return self.vid == other.vid
