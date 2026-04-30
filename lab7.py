8c4ff5930677eb7bb6eca6ae7e1a84267ca24b72
buurki <161019315+buurki@users.noreply.github.com>
2026-04-30 12:39:16 +0300

    Update lab7.py

--- lab7.py
+++ lab7.py
@@ -1,4 +1,4 @@
-lass Vehicle:
+class Vehicle:
     def __init__(self, vid, model, year):
         self.vid = vid
         self.model = model
@@ -12,3 +12,12 @@ lass Vehicle:
 
     def __eq__(self, other):
         return self.vid == other.vid
+
+class Car(Vehicle):
+    def __init__(self,vid,model,year,fuel,doors):
+        super().__init__(vid,model,year)
+        self.fuel= fuel
+        self.doors=doors
+
+    def __str__(self):
+        return f"[Car] {self.vid }|{self.model} |{self.year} | {self.fuel} | {self.doors}doors"
